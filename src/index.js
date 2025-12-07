7c733096006eb5de24d5fa8fd6468c451a3645f1
shaw-samina <jinxcapsule@yahoo.com>
2025-12-07 00:02:08 -0500

    Modified language, improved user experience.

--- src/index.js
+++ src/index.js
@@ -13,8 +13,11 @@ function createPoem(event) {
   let userData = document.querySelector("#user-data");
   let apiKey = "f0a4tbb1fc986b4d534ccb04b8291ao0";
   let prompt = `generate a poem about ${userData.value}`;
-  let context = "stop at 20 words";
+  let context = "no more than 12 words total";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poem = document.querySelector("#poem");
+  poem.innerHTML = "Generating poem...";
 
   axios.get(apiUrl).then(displayPoem);
 }
