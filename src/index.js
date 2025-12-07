19fefc01334009a39ce4470d036a31e97b4ae8d8
shaw-samina <jinxcapsule@yahoo.com>
2025-12-06 22:17:18 -0500

    Added basic JS form handling, added typewriter plug in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function createPoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "a poem of your choosing",
+    autoStart: true,
+    delay: "natural",
+    cursor: "",
+  });
+}
+
+let poemForm = document.querySelector("#form");
+poemForm.addEventListener("submit", createPoem);
