fa1f46fa0e076e8122fc5e138facf7bea743e354
shaw-samina <jinxcapsule@yahoo.com>
2025-12-06 22:58:04 -0500

    added axios, API integration with prompts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function createPoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "a poem of your choosing",
+    strings: response.data.answer,
     autoStart: true,
     delay: "natural",
     cursor: "",
   });
+}
+
+function createPoem(event) {
+  event.preventDefault();
+
+  let userData = document.querySelector("#user-data");
+  let apiKey = "f0a4tbb1fc986b4d534ccb04b8291ao0";
+  let prompt = `generate a poem about ${userData.value}`;
+  let context = "stop at 20 words";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#form");
